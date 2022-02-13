3fd6fb2d3f91db3c034dd991b1ead02d19d28184
ababyduck <kassisaf@gmail.com>
2022-02-13 14:36:03 -0800

    Update example secrets.py for new deployment

--- afkcode/secrets.example.py
+++ afkcode/secrets.example.py
@@ -1,3 +1,7 @@
 from dj_database_url import parse
 
-db_info = parse('postgres://user:pass@localhost:5432/dbname')
+# Important: All of these must be updated for deployment
+secret_key = ''
+allowed_hosts = '127.0.0.1,localhost'.split(',')
+admins = [('Your django admin username', 'Your django admin email address')]
+prod_db_info = parse('postgres://user:pass@localhost:5432/dbname')
