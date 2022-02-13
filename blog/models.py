19b2b13aad47414758002ab0f126678324e5ebda
ababyduck <kassisaf@gmail.com>
2022-02-13 07:33:32 -0800

    Add slugs to blog

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 TIME_FORMAT = '%D, %T %Z'
 
@@ -19,9 +21,22 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+    slug = models.SlugField(default='', editable=False, max_length=255, null=False)
 
     def __str__(self):
         return f'{self.title} ({self.created_on.strftime(TIME_FORMAT)})'
+
+    def get_absolute_url(self):
+        kwargs = {
+            'pk': self.pk,
+            'slug': self.slug
+        }
+        return reverse('blog_detail', kwargs=kwargs)
+
+    def save(self, *args, **kwargs):
+        value = self.title
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from blog import views
 
 urlpatterns = [
     path('', views.blog_index, name='blog_index'),
-    path('<int:pk>/', views.blog_detail, name='blog_detail'),
-    path('<category>/', views.blog_category, name='blog_category')
+    # path('articles/<int:year>/', views.blog_detail, name='blog_years'),
+    # path('articles/<int:year>/<int:month>', views.blog_detail, name='blog_months'),
+    path('articles/<int:year>/<int:month>/<int:pk>-<slug:slug>', views.blog_detail, name='blog_detail'),
+    path('<category>/', views.blog_category, name='blog_category'),
 ]
