a779d8fb5268d20844619e480304b86e9034eb3a
ababyduck <kassisaf@gmail.com>
2022-03-01 12:23:29 -0800

    Enable code highlighting with pygments

--- blog/templatetags/markdownify.py
+++ blog/templatetags/markdownify.py
@@ -1,10 +1,23 @@
 from django import template
 import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
 
 register = template.Library()
 
 
+class HighlightRenderer(mistune.HTMLRenderer):
+    def block_code(self, code, lang=None):
+        if lang:
+            lexer = get_lexer_by_name(lang, stripall=True)
+            formatter = html.HtmlFormatter()
+            return highlight(code, lexer, formatter)
+        return f'<pre><code>{mistune.escape(code)}</pre></code>'
+
+
 @register.filter
 def markdown(value):
-    md = mistune.create_markdown(plugins=['strikethrough', 'task_lists'])
-    return md(value)
+    renderer = HighlightRenderer()
+    process = mistune.create_markdown(renderer=renderer, plugins=['strikethrough', 'task_lists'])
+    return process(value)
