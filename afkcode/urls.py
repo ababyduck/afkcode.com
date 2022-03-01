a960eae3f5da6efa40c039912a479a207382400f
ababyduck <kassisaf@gmail.com>
2022-02-28 17:01:38 -0800

    Change default admin path
    Security through obscurity!

--- afkcode/urls.py
+++ afkcode/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('obscurity/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('blog/', include('blog.urls')),
     path('courses/', include('courses.urls')),
