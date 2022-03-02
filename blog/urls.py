24df7696f84abc301152e55798798508daaf7a2a
ababyduck <kassisaf@gmail.com>
2022-03-02 00:43:05 -0800

    Fix comment form

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.blog_index, name='blog_index'),
     # path('articles/<int:year>/', views.blog_detail, name='blog_years'),
     # path('articles/<int:year>/<int:month>', views.blog_detail, name='blog_months'),
-    path('articles/<int:year>/<int:month>/<int:pk>-<slug:slug>', views.blog_detail, name='blog_detail'),
+    path('blog/<int:year>/<int:month>/<int:pk>-<slug:slug>', views.blog_detail, name='blog_detail'),
     path('category/<category>/', views.blog_category, name='blog_category'),
 ]
