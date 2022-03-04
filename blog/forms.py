dda10502ed2c50f2cf47f2365204ddb1e469753d
ababyduck <kassisaf@gmail.com>
2022-03-04 06:54:30 -0800

    Restyle comment form

--- blog/forms.py
+++ blog/forms.py
@@ -5,13 +5,16 @@ class CommentForm(forms.Form):
     author = forms.CharField(
         max_length=60,
         widget=forms.TextInput(attrs={
+            'id': 'comment-form-author',
             'class': 'form-control',
-            'placeholder': 'Your Name'
+            'placeholder': 'Your name'
         })
     )
     body = forms.CharField(widget=forms.Textarea(
         attrs={
+            'id': 'comment-form-body',
             'class': 'form-control',
-            'placeholder': 'Leave a comment!'
+            'placeholder': 'Leave a comment!',
+            'rows': 4
         })
     )
