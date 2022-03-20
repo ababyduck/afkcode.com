c689c1cfe2ac5a5925560830955432bcc92f0a1a
ababyduck <kassisaf@gmail.com>
2022-03-20 03:05:23 -0700

    Add url pattern to filter courses by school

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,6 @@ from courses import views
 
 urlpatterns = [
     path('', views.course_index, name='course_index'),
+    path('school=<filter_by_school>', views.course_index, name='course_index'),
     path('<int:pk>-<slug:slug>', views.course_detail, name='course_detail')
 ]
--- courses/views.py
+++ courses/views.py
@@ -3,12 +3,16 @@ from courses.models import Course
 from projects.models import Project
 
 
-def course_index(request):
-    all_courses = Course.objects.all()
+def course_index(request, filter_by_school=''):
+    if filter_by_school:
+        all_courses = Course.objects.filter(school__initials=filter_by_school.upper())
+    else:
+        all_courses = Course.objects.all()
     courses_in_progress = all_courses.filter(status=Course.Status.IN_PROGRESS).order_by('school__name', 'subject', 'code')
     courses_completed = all_courses.filter(status=Course.Status.COMPLETED).order_by('year', 'semester')
     courses_planned = all_courses.filter(status=Course.Status.PLANNED).order_by('year', 'semester')
     context = {
+
         'courses_in_progress': courses_in_progress,
         'courses_completed': courses_completed,
         'courses_planned': courses_planned
