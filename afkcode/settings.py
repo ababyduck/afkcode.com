c90b8ece015764925e7aaf518f8054faa40ae0d0
ababyduck <kassisaf@gmail.com>
2022-03-20 02:24:36 -0700

    Install htmx

--- afkcode/settings.py
+++ afkcode/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     'storages',
     'django_fastdev',
     'django_browser_reload',
+    'django_htmx',
     # My apps
     'projects',
     'blog',
@@ -96,6 +97,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
     # 'django_minify_html.middleware.MinifyHtmlMiddleware',
     'afkcode.middleware.CompliantMinifyHtmlMiddleware',
     'django_browser_reload.middleware.BrowserReloadMiddleware',  # Must come after any response-encoding middleware
